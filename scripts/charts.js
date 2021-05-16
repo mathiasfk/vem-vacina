be69580db9951705324ca90bdab62b572bff908e
Mathias Kriebel <mathias.kriebel@bwtech.com>
2021-05-16 13:07:16 -0300

    Adding new bar chart

--- scripts/charts.js
+++ scripts/charts.js
@@ -2,7 +2,7 @@ const getFieldArray = function(data, field) {
     return data.map(each => each[field]);
 }
 
-const plotChart = function(chartId, data, fields) {
+const plotLineChart = function(chartId, data, fields) {
     const ctx = document.getElementById(chartId).getContext("2d");
     new Chart(ctx, {
       type: "line",
@@ -42,4 +42,50 @@ const plotChart = function(chartId, data, fields) {
         }
       },
     });
+};
+
+const plotBarChart = function(chartId, data) {
+  const ctx = document.getElementById(chartId).getContext("2d");
+
+  const chartData = {
+    datasets: [
+      {
+        label: data.fully_vaccinated.label,
+        data: [data.fully_vaccinated.value],
+        backgroundColor: data.fully_vaccinated.color,
+        stack: 0
+      },
+      {
+        label: data.vaccinated.label,
+        data: [data.vaccinated.value],
+        backgroundColor: data.vaccinated.color,
+        stack: 0
+      },
+      {
+        label: data.non_vaccinated.label,
+        data: [data.non_vaccinated.value],
+        backgroundColor: data.non_vaccinated.color,
+        stack: 0
+      }
+    ]
+  };
+
+  const config = {
+    type: 'horizontalBar',
+    data: chartData,
+    options: {
+      tooltips: {
+        callbacks: {
+          title: function(tooltipItem, data) {
+            return "Percentagem da população"
+          },
+          label: function(tooltipItem, data) {
+            return `${data.datasets[tooltipItem.datasetIndex].label}  ${formatNumber(data.datasets[tooltipItem.datasetIndex].data[0])}%`;
+          }
+        }
+      }
+    }
+  };
+
+  new Chart(ctx, config);
 };
